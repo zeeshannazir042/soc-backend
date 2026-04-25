f24242f62c4d8cf88a527f655f101743a5b42c82
Zeeshan Nazir <zeeshannazir042@gmail.com>
2026-04-25 22:35:07 +0200

    fix cors for production frontend

--- src/index.ts
+++ src/index.ts
@@ -9,18 +9,30 @@ import triage from "./routes/triage";
 import incidents from "./routes/incidents";
 
 dotenv.config();
+
 const app = express();
+
 app.use(cors({
-  origin: "http://localhost:5173"
+  origin: [
+    "http://localhost:5173",
+    "https://soc-frontend-seven.vercel.app/"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
 }));
+
 app.use(express.json());
+
+// DB
 connectDB();
 
+// Routes
 app.use("/alerts", alerts);
 app.use("/triage", triage);
 app.use("/incidents", incidents);
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
